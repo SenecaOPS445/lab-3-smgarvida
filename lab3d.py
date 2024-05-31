9824de6dd31cae92c2d8f6d0864fc83132bab005
Shayna Mae Garvida <smgarvida@myseneca.ca>
2024-05-31 07:57:43 -0400

    Added lab3d

---
+++ lab3d.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Author ID:smgarvida
+
+
+import subprocess
+
+def free_space():
+    free_space = subprocess.Popen("df -h | grep '/$' | awk '{print $4}'", stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output = free_space.communicate()
+    stdout = output[0].decode('utf-8').strip()
+    return stdout
+
+if __name__ == '__main__':
+    print(free_space())
